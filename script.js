923436b35d89a8835425789f654799c13b9592bf
Endrian-Apryono17 <endrianapryono17@gmail.com>
2023-01-05 22:51:29 +0700

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+let loaders = document.getElementById('container')
+    window.addEventListener("load", function() {
+        // setTimeout(function(){ document.body.removeChild(loaders);},3000) 
+        document.body.removeChild(loaders)
+    })
